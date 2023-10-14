7611627712d8e5c8f43e9333bb0066454a6bd687
Mohammad Rubel Mollik <rubel@pop-os.localdomain>
2023-10-15 02:13:09 +0600

    Sign in with google, open google login popup

--- src/steps.js
+++ src/steps.js
@@ -3,5 +3,10 @@
  *create a project on console.fiebase.google.com
  *install firebase
  *Register web app in firebase 
- 
+ *coppy firebase int with config from firebase project settings
+ *export default app
+ *import app firebase init.s into your app.js
+ *import getAuth from firebase/auth and create auth = getAuth(app)
+ *turn on google authentication (firebase >authentication> enable google sign in)
+ *create google provider
  */
